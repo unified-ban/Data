c2f2a0c683a36c9b29df9e3e7dfd1053fc022c73
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2019-09-25 19:00:53 +0200

    Change Operator's TelegramUserId from string to int and provide its service

---
+++ Unifiedban.Data/Migrations/20190925161958_Unifiedban-ChangeOperatorTelegramUserIdToInt.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Unifiedban.Data.Migrations
+{
+    public partial class UnifiedbanChangeOperatorTelegramUserIdToInt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TelegramUserId",
+                schema: "dbo",
+                table: "Operator",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TelegramUserId",
+                schema: "dbo",
+                table: "Operator",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}
---
+++ Unifiedban.Data/OperatorService.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Unifiedban.Models;
+
+namespace Unifiedban.Data
+{
+    public class OperatorService
+    {
+        public Operator Add(Operator oper, int callerId)
+        {
+            using (UBContext ubc = new UBContext())
+            {
+                try
+                {
+                    ubc.Add(oper);
+                    ubc.SaveChanges();
+                    return oper;
+                }
+                catch (Exception ex)
+                {
+                    Utils.Logging.AddLog(new SystemLog()
+                    {
+                        LoggerName = "Unifiedban",
+                        Date = DateTime.Now,
+                        Function = "Unifiedban.Data.OperatorService.Add",
+                        Level = SystemLog.Levels.Warn,
+                        Message = ex.Message,
+                        UserId = callerId
+                    });
+                    if (ex.InnerException != null)
+                        Utils.Logging.AddLog(new SystemLog()
+                        {
+                            LoggerName = "Unifiedban.Data",
+                            Date = DateTime.Now,
+                            Function = "Unifiedban.Data.OperatorService.Add",
+                            Level = SystemLog.Levels.Warn,
+                            Message = ex.InnerException.Message,
+                            UserId = callerId
+                        });
+                }
+                return null;
+            }
+        }
+        public Operator Update(Operator oper, int callerId)
+        {
+            using (UBContext ubc = new UBContext())
+            {
+                Operator exists = ubc.Operators
+                    .Where(x => x.OperatorId == oper.OperatorId)
+                    .FirstOrDefault();
+                if (exists == null)
+                    return null;
+
+                try
+                {
+                    exists.TelegramUserId = oper.TelegramUserId;
+                    exists.Level = oper.Level;
+
+                    ubc.SaveChanges();
+                    return oper;
+                }
+                catch (Exception ex)
+                {
+                    Utils.Logging.AddLog(new SystemLog()
+                    {
+                        LoggerName = "Unifiedban",
+                        Date = DateTime.Now,
+                        Function = "Unifiedban.Data.OperatorService.Update",
+                        Level = SystemLog.Levels.Warn,
+                        Message = ex.Message,
+                        UserId = callerId
+                    });
+                    if (ex.InnerException != null)
+                        Utils.Logging.AddLog(new SystemLog()
+                        {
+                            LoggerName = "Unifiedban.Data",
+                            Date = DateTime.Now,
+                            Function = "Unifiedban.Data.OperatorService.Update",
+                            Level = SystemLog.Levels.Warn,
+                            Message = ex.InnerException.Message,
+                            UserId = callerId
+                        });
+                }
+                return null;
+            }
+        }
+        public SystemLog.ErrorCodes Remove(Operator oper, int callerId)
+        {
+            using (UBContext ubc = new UBContext())
+            {
+                Operator exists = ubc.Operators
+                    .Where(x => x.OperatorId == oper.OperatorId)
+                    .FirstOrDefault();
+                if (exists == null)
+                    return SystemLog.ErrorCodes.Error;
+
+                try
+                {
+                    ubc.SaveChanges();
+                    return SystemLog.ErrorCodes.OK;
+                }
+                catch (Exception ex)
+                {
+                    Utils.Logging.AddLog(new SystemLog()
+                    {
+                        LoggerName = "Unifiedban",
+                        Date = DateTime.Now,
+                        Function = "Unifiedban.Data.OperatorService.Remove",
+                        Level = SystemLog.Levels.Warn,
+                        Message = ex.Message,
+                        UserId = callerId
+                    });
+                    if (ex.InnerException != null)
+                        Utils.Logging.AddLog(new SystemLog()
+                        {
+                            LoggerName = "Unifiedban.Data",
+                            Date = DateTime.Now,
+                            Function = "Unifiedban.Data.OperatorService.Remove",
+                            Level = SystemLog.Levels.Warn,
+                            Message = ex.InnerException.Message,
+                            UserId = callerId
+                        });
+                }
+                return SystemLog.ErrorCodes.Error;
+            }
+        }
+        public List<Operator> Get(Expression<Func<Operator, bool>> whereClause)
+        {
+            using (UBContext ubc = new UBContext())
+            {
+                if (whereClause == null)
+                    return ubc.Operators
+                        .AsNoTracking()
+                        .ToList();
+
+                return ubc.Operators
+                    .AsNoTracking()
+                    .Where(whereClause)
+                    .ToList();
+
+            }
+        }
+    }
+}
