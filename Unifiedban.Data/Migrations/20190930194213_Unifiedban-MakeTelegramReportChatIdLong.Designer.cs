54e024cb8d43f641c98ee22ffdf6b57fbb6d43d3
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2019-09-30 21:47:06 +0200

    Make Group's ReportChatId long

--- Unifiedban.Data/Migrations/20190930194213_Unifiedban-MakeTelegramReportChatIdLong.Designer.cs
+++ Unifiedban.Data/Migrations/20190930194213_Unifiedban-MakeTelegramReportChatIdLong.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Unifiedban.Data;
 
 namespace Unifiedban.Data.Migrations
 {
     [DbContext(typeof(UBContext))]
-    partial class UBContextModelSnapshot : ModelSnapshot
+    [Migration("20190930194213_Unifiedban-MakeTelegramReportChatIdLong")]
+    partial class UnifiedbanMakeTelegramReportChatIdLong
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
