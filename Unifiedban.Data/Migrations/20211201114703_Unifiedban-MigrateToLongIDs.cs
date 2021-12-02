bbf2d19e6802f26da158579c9967fba7374ce0a1
Pietro di Caprio <pietro@fabricators.ltd>
2021-12-02 18:03:32 +0100

    Update Telegram lib for int64 IDs

---
+++ Unifiedban.Data/Migrations/20211201114703_Unifiedban-MigrateToLongIDs.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Unifiedban.Data.Migrations
+{
+    public partial class UnifiedbanMigrateToLongIDs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ActionTakenBy",
+                schema: "log",
+                table: "TrustFactorLog",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "SenderId",
+                schema: "log",
+                table: "SupportSession",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "TelegramUserId",
+                schema: "log",
+                table: "Operation",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "TelegramUserId",
+                schema: "dbo",
+                table: "User_TrustFactor",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "TelegramUserId",
+                schema: "dbo",
+                table: "User_Banned",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "TelegramUserId",
+                schema: "dbo",
+                table: "Group_DashboardUser",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ActionTakenBy",
+                schema: "log",
+                table: "TrustFactorLog",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SenderId",
+                schema: "log",
+                table: "SupportSession",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TelegramUserId",
+                schema: "log",
+                table: "Operation",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TelegramUserId",
+                schema: "dbo",
+                table: "User_TrustFactor",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TelegramUserId",
+                schema: "dbo",
+                table: "User_Banned",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TelegramUserId",
+                schema: "dbo",
+                table: "Group_DashboardUser",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long));
+        }
+    }
+}
