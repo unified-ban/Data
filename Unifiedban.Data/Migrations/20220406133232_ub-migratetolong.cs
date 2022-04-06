b6de96b920cc6dffb45ce2f06c3a592a7b013601
Pietro di Caprio <pietro@fabricators.ltd>
2022-04-06 17:02:05 +0200

    Telegram user id to long

---
+++ Unifiedban.Data/Migrations/20220406133232_ub-migratetolong.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Unifiedban.Data.Migrations
+{
+    public partial class ubmigratetolong : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "TelegramUserId",
+                schema: "dbo",
+                table: "DashboardSession",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TelegramUserId",
+                schema: "dbo",
+                table: "DashboardSession",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long));
+        }
+    }
+}
